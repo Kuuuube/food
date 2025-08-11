59e74d33786d634c48b54b13c6d174d8492925b6
kuuuube <hexagonisalie@gmail.com>
2025-08-11 00:51:17 -0400

    Use venv for test_deploy.sh and add testing

---
+++ test.sh
@@ -0,0 +1,2 @@
+source .env/bin/activate
+python -m pytest tests/tests.py -vv
--- test_deploy.sh
+++ test_deploy.sh
@@ -1,3 +1,4 @@
 cd $(dirname "$0")
+source .env/bin/activate
 python build.py
 python -m http.server --bind localhost --directory food
