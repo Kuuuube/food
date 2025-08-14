408d59a47260546fafa8174c81f318fc5c8efd09
kuuuube <hexagonisalie@gmail.com>
2025-08-14 14:11:33 -0400

    Add scoring for search

--- page_assets/search.js
+++ page_assets/search.js
@@ -1,15 +1,45 @@
+function truncate_string(input_string) {
+    return input_string.toLowerCase().replace(/[-_]/, " ").trim().replaceAll(/[^a-zA-Z0-9\s]/g, "");
+}
+
 function update_search() {
-    const search_text_lowered = document.querySelector("#index-page-search").value.toLowerCase();
+    const search_text_truncated = truncate_string(document.querySelector("#index-page-search").value);
     let possible_results = [];
     for (const search_item of SEARCH_INDEX) {
-        if (search_text_lowered.length === 0) { break; }
-        const index_name_lowered = search_item.name.toLowerCase();
-        if (index_name_lowered === search_text_lowered) {
-            possible_results.splice(0, 0, search_item);
-        } else if (index_name_lowered.includes(search_text_lowered)) {
-            possible_results.push(search_item);
+        if (search_text_truncated.length === 0) { break; }
+        const index_name_truncated = truncate_string(search_item.name);
+
+        let score = 0;
+
+        if (index_name_truncated.includes(search_text_truncated)) {
+            score += 1; // partial any position match
+        }
+        const partial_any_word_start = new RegExp(`(^| )${RegExp.escape(search_text_truncated)}`);
+        if (index_name_truncated.search(partial_any_word_start) !== -1) {
+            score += 10; // partial any word start match
+        }
+        const partial_start = new RegExp(`^${RegExp.escape(search_text_truncated)}`);
+        if (index_name_truncated.search(partial_start) !== -1) {
+            score += 100; // partial start match
+        }
+        const full_any_word = new RegExp(`(^| )${RegExp.escape(search_text_truncated)}( |$)`);
+        if (index_name_truncated.search(full_any_word) !== -1) {
+            score += 1000; // full any word match
+        }
+        const full_start_word = new RegExp(`^${RegExp.escape(search_text_truncated)}( |$)`)
+        if (index_name_truncated.search(full_start_word) !== -1) {
+            score += 10000; // full word start match
+        }
+        if (index_name_truncated === search_text_truncated) {
+            score += 100000; // entire search exact match
+        }
+
+        if (score > 0) {
+            possible_results.push({item: search_item, score: score});
         }
     }
+
+    possible_results.sort((a, b) => {return b.score - a.score});
 
     const search_results = document.querySelectorAll(".search-result");
     for (const search_result of search_results) {
@@ -21,8 +51,8 @@ function update_search() {
             search_result.classList.add("inactive-result");
             continue;
         }
-        search_result.textContent = result.name;
-        search_result.href = result.path;
+        search_result.textContent = result.item.name;
+        search_result.href = result.item.path;
     }
 }
 
